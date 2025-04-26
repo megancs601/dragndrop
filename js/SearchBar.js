a8c503a9cbcb4319b6d411e47b22f36f7b695997
Megan C. Smith <megan.cs601@gmail.com>
2025-04-26 13:07:09 -0400

    attempt to validate url

--- js/SearchBar.js
+++ js/SearchBar.js
@@ -4,6 +4,7 @@ export default class SearchBar {
     constructor(uploadedSec) {
         this.textInput = document.querySelector('#text-input');
         this.searchBtn = document.querySelector('#search-btn');
+        this.errorMessage = document.querySelector('#error-message');
         this.uploadedSec = uploadedSec;
         this.addEventListeners();
     }
@@ -14,17 +15,46 @@ export default class SearchBar {
         );
     }
 
-    // TODO: validate img url.
-    // RESEARCH: Cheerio or other HTML parsers
-    // NOTE: best done using a search engine other than google
-    searchBtnClickedHandler() {
+    // NOTE: image urls best copied from a search engine other than google
+    async searchBtnClickedHandler() {
+        this.errorMessage.innerText = '';
+
         if (this.textInput.value.trim().length) {
-            const content = ImageUploader.createContentContainer(
-                undefined,
-                this.textInput.value
-            );
-            this.uploadedSec?.appendChild(content);
-            this.textInput.value = '';
+            const validImage = await this.isValidImage();
+
+            if (validImage) {
+                const content = ImageUploader.createContentContainer(
+                    undefined,
+                    this.textInput.value
+                );
+                this.uploadedSec?.appendChild(content);
+            } else {
+                this.errorMessage.innerText =
+                    'Sorry, image could not be uploaded';
+            }
+        }
+
+        this.textInput.value = '';
+    }
+
+    async isValidImage() {
+        try {
+            const response = await fetch(this.textInput.value.trim());
+            if (!response.ok) {
+                console.log('unsuccessful request');
+                return false;
+            }
+
+            const buff = await response.blob();
+            if (buff.type.startsWith('image/')) {
+                return true;
+            }
+
+            console.log('url is not of type image');
+            return false;
+        } catch (error) {
+            console.log(error);
+            return false;
         }
     }
 }
